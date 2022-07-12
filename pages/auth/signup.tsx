dae70783c061edfb5a2e907c66c6f0338bea074f
YusunPark <solarpy@hanyang.ac.kr>
2022-07-12 13:39:59 +0900

    Update signup page

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,7 +1,20 @@
 import styled from "@emotion/styled";
 
 export default function SignupPage() {
-  return <Wrapper></Wrapper>;
+  return (
+    <>
+      <Wrapper>
+        <p>회원정보를 입력해주세요</p>
+        <div></div>
+        <Line />
+        <p>쿠팡 서비스약관에 동의해주세요</p>
+      </Wrapper>
+    </>
+  );
 }
 
 const Wrapper = styled.div``;
+
+const Line = styled.div`
+  border: 1px solid #ccc;
+`;
